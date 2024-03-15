112101c8539c8df1ff2422e064542043eae14f62
RenanAAraujo <renanarauj5@gmail.com>
2024-03-15 15:06:30 -0300

    Subindo o projeto de CRUD construido com tailwindcss, typescript e react

---
+++ src/controllers/DeleteCustomerController.ts
@@ -0,0 +1,17 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { DeleteCustomerService } from '../services/DeleteCustomerService'
+
+class DeleteCustomerController {
+    async handle(request: FastifyRequest, replay: FastifyReply){
+
+        const { id } = request.query as { id: string}
+
+        const customerService = new DeleteCustomerService();
+
+        const customer = await customerService.execute({id})
+
+        replay.send(customer)
+    }
+}
+
+export { DeleteCustomerController }
